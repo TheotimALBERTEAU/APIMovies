0228364d4e94bc6fce3e1efa334f1b193277847a
TheotimALBERTEAU <149970439+TheotimALBERTEAU@users.noreply.github.com>
2025-12-07 19:07:51 +0100

    Add Core folder

--- models/Movies.model.js
+++ models/Movies.model.js
@@ -1,3 +1,60 @@
 const mongoose = require('mongoose')
 
 const { moviesConn} = require('../app');
+
+const moviesSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Le titre est obligatoire.'],
+        unique: true,
+        trim: true,
+    },
+    year: {
+        type: Number,
+        required: [true, "L'année de sortie est obligatoire."],
+        min: 1900,
+        max: new Date().getFullYear(),
+    },
+    author: {
+        type: String,
+        required: [true, "L'auteur (réalisateur) est obligatoire."],
+        trim: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    genre: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    synopsis: {
+        type: String,
+        required: true,
+        maxlength: 1000,
+    },
+    cover: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0,
+    },
+    trailer: {
+        type: String,
+    },
+    link: {
+        type: String,
+        trim: true,
+        unique: true,
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = moviesConn.model('Movies', moviesSchema);
--- movies/movies-routes.js
+++ movies/movies-routes.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { httpApiResponse } = require('../core/http-library');
+
+const Movies = require('../models/Movies.model');
+
+router.get('/', async (request, response) => {
+    try {
+        const movies = await Movies.find();
+        
+        return httpApiResponse(response, "200", "Movies list recovered", movies);
+    } catch (error) {
+        console.error("Error when recovering movies", error);
+        return httpApiResponse(response, "500", "Server Error when movies recovering", null);
+    }
+});
+
+
