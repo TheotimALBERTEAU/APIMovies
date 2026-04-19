eb16d06f67fa5dee601689c94955dbe52cca8152
TheotimALBERTEAU <149970439+TheotimALBERTEAU@users.noreply.github.com>
2026-04-19 13:20:07 +0200

    🚀 up : config cors | app

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (!MONGODB_URI) {
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
